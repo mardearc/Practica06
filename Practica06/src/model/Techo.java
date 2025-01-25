41b8afd39eb58528c13a152cc0ffc9c40d3d60b8
mardearc <jesumar01@gmail.com>
2025-01-25 11:17:21 +0100

    Techo

--- Practica06/src/model/Techo.java
+++ Practica06/src/model/Techo.java
@@ -1,20 +1,36 @@
 package model;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Techo {
     private int x, y, ancho, altura;
+    private Image spriteTecho;
 
     public Techo(int x, int y, int ancho, int altura) {
         this.x = x;
         this.y = y;
         this.ancho = ancho;
         this.altura = altura;
+        
+        File archivoTecho = new File("images/techo.png");
+        
+        try {
+			BufferedImage imagenTecho = ImageIO.read(archivoTecho);
+			
+			spriteTecho = imagenTecho;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     public void dibujar(Graphics g) {
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(x, y, ancho, altura);
+    	g.drawImage(spriteTecho, x, y, ancho, altura, null);
     }
 
     public int getAltura() {
